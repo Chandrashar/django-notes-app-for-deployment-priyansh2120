ff1e8e2701121b61a253aa5a62184ac7d78ecfad
csharma@classicvacations.com <csharma@classicvacations.com>
2026-09-09 04:10:01 +0530

    commented notes-app entry in urls.py

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('notes-app/', include('notes-app.urls')),
+    #path('notes-app/', include('notes-app.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
